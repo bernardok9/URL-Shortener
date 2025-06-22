aaf5eb4de6fc3620acea2ee7901966a31ae18a61
bernardok9 <m1t0.fearless@gmail.com>
2025-06-21 22:32:44 -0300

    chore: add docker compose and database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { envValidationSchema } from './config/env.validation';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    validationSchema: envValidationSchema,
+  }),
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.DATABASE_HOST,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,    
+    port: Number(process.env.DATABASE_PORT),
+    database: process.env.DATABASE_NAME,  
+    autoLoadEntities: true,
+    synchronize: true,
+  }),
+],
+
   controllers: [AppController],
   providers: [AppService],
 })
