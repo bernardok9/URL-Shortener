ded833c06d540058b0a9482ec4002fce194f04c9
bernardok9 <m1t0.fearless@gmail.com>
2025-06-23 13:28:41 -0300

    feat: add swagger endpoint on main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
+
 
 async function bootstrap() {
+  dotenv.config();
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // if Swagger enabled the address is localhost:3000/api
+  if (process.env.SWAGGER_ENABLED?.toLowerCase() === 'true') {
+    const config = new DocumentBuilder()
+      .setTitle('Backend Test')
+      .setDescription('API Documentation')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
+
+  await app.listen(3000);
 }
 bootstrap();
